e66f239b49a80b86f113607265989f174e91d9e0
kailana <kailanakahawaii@gmail.com>
2020-08-29 20:36:59 -0700

    first commit

---
+++ queues.js
@@ -0,0 +1,11 @@
+//array
+
+var queue = []
+
+queue.push('first')
+queue.push('second')
+queue.push('third')
+
+queue.shift()
+queue.shift()
+queue.shift()
---
+++ stacks.js
@@ -0,0 +1,13 @@
+//array
+
+var stack = []
+
+stack.push("google.com")
+
+stack.push("instagram")
+
+stack.push("youtube")
+
+stack.pop()
+
+stack.push("amazon")
